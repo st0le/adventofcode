c46465aec5f34ddfa63c73ea8c0d10c9e703533e
st0le <gaurav.kamath@outlook.com>
2020-12-05 16:49:26 -0800

    Added Problem 5

---
+++ 2020/dotnet-csharp/5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Go();
+        }
+
+        public void Go()
+        {
+            // Unit Test
+            // Console.WriteLine(Decode("BFFFBBFRRR"));
+            // Console.WriteLine(Decode("FFFBBBFRRR"));
+            // Console.WriteLine(Decode("BBFFBBFRLL"));
+            string line;
+            int minId = int.MaxValue, maxId = int.MinValue;
+            long sum = 0;
+            while ((line = Console.ReadLine()) != null)
+            {
+                int id = Decode(line);
+                sum += id;
+                maxId = Math.Max(maxId, id);
+                minId = Math.Min(minId, id);
+            }
+            Console.WriteLine(maxId);
+            Console.WriteLine((((minId + maxId) * (maxId - minId + 1)) / 2) - sum);
+        }
+
+        private int Decode(string s)
+        {
+            int row = 0, col = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                if (s[i] == 'B')
+                {
+                    row |= 1;
+                }
+                row <<= 1;
+            }
+            row >>= 1;
+            for (int i = 7; i < 10; i++)
+            {
+                if (s[i] == 'R')
+                {
+                    col |= 1;
+                }
+                col <<= 1;
+            }
+            col >>= 1;
+            return (row << 3) | col;
+        }
+    }
+}
