2421c5198e58e394dd8be8cc756713d4bc6c0f4c
st0le <gaurav.kamath@outlook.com>
2020-12-07 12:42:25 -0800

    Add 6 and 7

---
+++ 2020/dotnet-csharp/6/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int part1 = 0, part2 = 0, people = 0;
+            Dictionary<char, int> answers = new Dictionary<char, int>();
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    // group ends
+                    part1 += answers.Count;
+
+                    foreach (var kv in answers)
+                    {
+                        if (kv.Value == people)
+                        {
+                            part2++;
+                        }
+                    }
+
+                    answers.Clear();
+                    people = 0;
+                }
+                else
+                {
+                    people++;
+                    foreach (char c in line)
+                    {
+                        if (!answers.TryGetValue(c, out int v))
+                        {
+                            answers[c] = 1;
+                        }
+                        else
+                        {
+                            answers[c] = v + 1;
+                        }
+                    }
+                }
+            }
+
+            // group ends
+            part1 += answers.Count;
+            foreach (var kv in answers)
+            {
+                if (kv.Value == people)
+                {
+                    part2++;
+                }
+            }
+            answers.Clear();
+
+            Console.WriteLine(part1);
+            Console.WriteLine(part2);
+        }
+    }
+}
---
+++ 2020/dotnet-csharp/7/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<Tuple<string, int>>> rules = new();
+            string line;
+            while (null != (line = Console.ReadLine()))
+            {
+                string container = line.Substring(0, line.IndexOf(" bags"));
+                var list = new List<Tuple<string, int>>();
+                rules.Add(container, list);
+                foreach (Match m in Regex.Matches(line, @"(\d+) (\w+\s\w+)"))
+                {
+                    int count = int.Parse(m.Groups[1].Value);
+                    string smallerBag = m.Groups[2].Value;
+                    list.Add(Tuple.Create(smallerBag, count));
+                }
+            }
+
+
+            // foreach (var item in rules)
+            // {
+            //     Console.WriteLine(item.Key);
+            // }
+            Console.WriteLine(rules.Keys.Where(bag => bag != "shiny gold" && CanContain(rules, bag, "shiny gold")).Count());
+            Console.WriteLine(CalculateBags(rules, "shiny gold") - 1);
+        }
+
+        static bool CanContain(Dictionary<string, List<Tuple<string, int>>> rules, string sourceBag, string targetBag)
+        {
+            if (sourceBag == targetBag)
+            {
+                return true;
+            }
+
+            foreach (var item in rules[sourceBag])
+            {
+                if (CanContain(rules, item.Item1, targetBag))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static int CalculateBags(Dictionary<string, List<Tuple<string, int>>> rules, string bag)
+        {
+            int s = 1;
+            foreach (var item in rules[bag])
+            {
+                s += item.Item2 * CalculateBags(rules, item.Item1);
+            }
+
+            return s;
+        }
+    }
+}
